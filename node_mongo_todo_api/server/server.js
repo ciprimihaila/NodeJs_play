a48b1d510396c589fd12fab5c47625426258f817
Ciprian Mihaila <cipri.mihaila@gmail.com>
2018-04-28 14:25:23 +0300

    add authentication middleware

--- node_mongo_todo_api/server/server.js
+++ node_mongo_todo_api/server/server.js
@@ -6,7 +6,8 @@ const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo')
-var {User} = require('./models/user')
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -91,6 +92,8 @@ app.patch('/todos/:id', (req, res) => {
         })
 })
 
+//post users
+
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body)
@@ -103,6 +106,10 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     });  
 });
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log('Started on port ' + port);
